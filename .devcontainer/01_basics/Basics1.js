ac853e9e27107814df025be31a619705ccc1cb91
Aquib-Sayyed <70154625+Aquib-Sayyed@users.noreply.github.com>
2025-07-11 12:45:38 +0000

    new commit

---
+++ .devcontainer/01_basics/Basics1.js
@@ -0,0 +1,17 @@
+const accountid=14453;
+let accountemail= "test@gmail.com";
+var accountpassword = "123456";
+
+let age= "12";
+
+console.log(typeof age);
+
+
+//accountid=9963;
+
+console.log(accountid);
+
+//typeof undefined will be undefined
+//typeof null will be object
+
+
---
+++ .devcontainer/01_basics/Basics_2.js
@@ -0,0 +1,27 @@
+let score=false;
+
+console.log(typeof score);
+
+let valuenumber = Number(score);
+
+console.log(typeof valuenumber);
+console.log(valuenumber);
+
+/*
+"33" => 33
+"33abc" => NaN
+true 1; false 0
+*/ 
+
+
+let score1=-33;
+
+var B = Boolean(score1)
+console.log(B);
+
+/*
+1 => true;
+0 => false
+"test" => true
+*/
+
