2efde8a8810336d5c1852e2daf82a6670a7a81b4
Aquib-Sayyed <70154625+Aquib-Sayyed@users.noreply.github.com>
2025-07-12 13:23:57 +0000

    datetime

---
+++ .devcontainer/01_basics/Numbers_and_Maths.js
@@ -0,0 +1,18 @@
+    let mydate=new Date()
+    console.log(mydate);
+
+    console.log(typeof mydate);
+
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(typeof mydate);
+
+
+    let mydate1=new Date("01-14-2023")
+    
+    console.log(mydate1);
+
+    let mytimestamp1  = Date.now()
+
+    console.log(mytimestamp1.getTime());
