3c52777f9ab88268da6d490ca14ad46d300021f1
Aquib-Sayyed <70154625+Aquib-Sayyed@users.noreply.github.com>
2025-07-12 11:42:08 +0000

    string ops

---
+++ .devcontainer/01_basics/Stringops.js
@@ -0,0 +1,27 @@
+const name='hitesh'
+const RepoCount=80
+
+console.log('hello my name is ${name} and my repo count is ${RepoCount}}')
+
+const gamename = new String('sayyed');
+
+console.log(gamename[0]);
+
+console.log(gamename.charAt(4));
+
+console.log(gamename);
+
+const B = gamename.substring(0,5)
+
+console.log(B);
+
+
+const C = gamename.slice(0,5)
+
+console.log(C);
+
+const D = "This is aquib sayyed i am software engineer";
+
+const E = D.includes('saquib');
+
+console.log(E);
