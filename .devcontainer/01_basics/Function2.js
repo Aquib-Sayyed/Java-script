fca215c8529e30e63886c69b26db57b139206666
Aquib-Sayyed <70154625+Aquib-Sayyed@users.noreply.github.com>
2025-07-23 19:48:55 +0000

    commit 2025-07-24

---
+++ .devcontainer/01_basics/Function2.js
@@ -0,0 +1,18 @@
+
+
+const user = {
+    username : "hitesh",
+    price : 999,
+
+WelcomeMessage: function() {
+
+    console.log(`${this.username}, Welcome to  `);
+    console.log(this);
+}
+
+}
+
+user.username="Sam"
+console.log(this); 
+
+
---
+++ .devcontainer/01_basics/codeflowinjs.js
@@ -0,0 +1,32 @@
+// <,>, <=,>=,==,!=,===
+
+const userloggedin = true
+const debicard = true
+const LoggedInFromEmail = true
+const LoggedInFromgoogle = true
+if(userloggedin && debicard && 2==3)
+{
+console.log("Allow to buy course");
+
+}
+
+if(LoggedInFromEmail || LoggedInFromgoogle ){
+
+
+}
+
+const useremail = "hitesh.ai"
+
+if(useremail)
+{
+    console.log("Got email user")
+}else    
+{
+    console.log("Dont have user email")
+}
+
+// falsy values
+
+false,0,-0,BigInt 0n,"",null,undefined,NaN
+
+// apart from these all values are true values
