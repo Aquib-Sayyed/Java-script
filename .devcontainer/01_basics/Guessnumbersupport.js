3de81ffc52d5672a1201d44d8484cda4868e3b0e
Aquib-Sayyed <70154625+Aquib-Sayyed@users.noreply.github.com>
2025-08-01 14:03:08 +0000

    GuessNumber commit

--- .devcontainer/01_basics/Guessnumbersupport.js
+++ .devcontainer/01_basics/Guessnumbersupport.js
@@ -14,24 +14,68 @@ let playGame=   true;
 
 if(playGame== true)
 {
-    const guess = parseInt(UserInput.value);
-    validateGuess(guess);
+subt.addEventListener('click', function(e){
+    e.preventDefault()
+    const guess = parseInt(UserInput)
+     validateGuess(guess)
+})
 }
 
 function validateGuess(guess)
 {
 // this function will validate only all the guesses is under 100, We will call al the function inside this
+if(isNaN(guess)){
+    alert('Kindly enter a valid number')
+}else if(guess<1)
+{
+    alert('Kindly enter a number more then 1')
+}else if(guess>100)
+{
+    alert('Kindly enter a number less then 100')
+}else{
+    prevGuess.push(guess)
+    if(numGuess === 11)
+    {
+DisplayGuesses(guess)
+DisplayMessage(`Game over. Random number was ${randomnumber}`)
+endgame()
+    }else{
+        DisplayGuesses(guess)
+        CheckGuess(guess)
+    }
+}
+
+
 }
 
 function CheckGuess(guess)
 {
 // this function will check this is equal to random or not and other things if it is equal
 //  function should display you won the game
+if(guess === randomnumber){
+    DisplayMessage('You guessed it right')
+    endgame()
+}
+else if(guess < randomnumber){
+    DisplayMessage('You guess is too low')
+    endgame()
+}
+else if(guess > randomnumber){
+    DisplayMessage('You guessed is too high')
+    endgame()
+}
+
 }
 
 function DisplayGuesses(Message)
 {
 // it will clean the value update the variables and label
+
+UserInput.value='';
+GuessesSlot.innerHTML += `${guess}`
+numGuess++
+remaining.innerHTML = `${11- numGuess}`
+
 }
 
 function DisplayMessage(Message)
